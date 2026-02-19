0e5a57b3a643fe7541af7efb9c3a34a0749157b5
tanish-sd26 <mauryatanisha824@gmail.com>
2026-02-20 00:28:32 +0530

    import module function

---
+++ functions/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  extends: [
+    "eslint:recommended",
+  ],
+  rules: {
+    "no-unused-vars": "warn",
+    "no-console": "off",
+  },
+};
